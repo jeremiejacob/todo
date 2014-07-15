2e022edd39a8678cec773e6f746260f906b22b60
jeremie jacob <j.jacob@bizwind.co.jp>
2014-07-15 16:30:16 +0800

    implement findAllCategorys()

--- src/main/java/com/roo/todo/service/CategoryService.java
+++ src/main/java/com/roo/todo/service/CategoryService.java
@@ -2,6 +2,9 @@ package com.roo.todo.service;
 
 import java.util.List;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
 import org.springframework.stereotype.Service;
 
 import com.roo.todo.entity.Category;
@@ -9,8 +12,11 @@ import com.roo.todo.entity.Category;
 @Service
 public class CategoryService {
 
+	@PersistenceContext
+	private EntityManager entityManager;
+	
 	public List<Category> findAllCategorys() {
-		return null;
+		return entityManager.createQuery("SELECT o FROM Category o", Category.class).getResultList();
 	}
 
 	public Category findCategory(Integer id) {
