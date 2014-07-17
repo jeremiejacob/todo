285b4ffb4fa811cfea2f73a4e6911297daa52404
jeremie jacob <j.jacob@bizwind.co.jp>
2014-07-17 14:06:49 +0800

    Refactor on TaskEditForm. convert date fields to string instead of Date and just format it before saving to database as well as showing the data from entity to toSring format

--- src/main/java/com/roo/form/TaskEditForm.java
+++ src/main/java/com/roo/form/TaskEditForm.java
@@ -1,11 +1,15 @@
 package com.roo.form;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 import javax.validation.constraints.NotNull;
 
+import org.apache.commons.lang3.time.DateUtils;
 import org.apache.log4j.Logger;
 import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.NotBlank;
 import org.hibernate.validator.constraints.NotEmpty;
 import org.springframework.roo.addon.javabean.RooJavaBean;
 
@@ -19,34 +23,42 @@ public class TaskEditForm {
 	
 	private Integer id;
 
-	@NotNull
-	private Date startTime;
+	@NotEmpty
+	private String startTime;
 
-	@NotNull
-	private Date endTime;
+	private String endTime;
+
+	private Integer categoryId;
 	
-	private Category category;
+	private Boolean status;
 
 	@NotEmpty
 	@Length(max = 50)
 	private String description;
 	
 	public Task toEntity() {
+		
 		Task task = new Task();
 		task.setId(id);
-		task.setStartTime(startTime);
-		task.setEndTime(endTime);
+		try {
+			task.setStartTime(DateUtils.parseDate(startTime, "YYYY-MM-dd HH:mm:ss"));
+			task.setEndTime(DateUtils.parseDate(endTime, "YYYY-MM-dd HH:mm:ss"));
+		} catch (ParseException e) {
+			task.setStartTime(new Date());
+			task.setEndTime(new Date());
+		}
+		
 		task.setDescription(description);
-		task.setCategory(category);
+		task.setCategory(new Category(){{setId(categoryId);}});
 		return task;
 	}
 	
 	public static TaskEditForm fromEntity(Task task) {
 		TaskEditForm form  = new TaskEditForm();
 		form.id = task.getId();
-		form.startTime = task.getStartTime();
-		form.endTime = task.getEndTime();
-		form.category = task.getCategory();
+		form.startTime = task.getStartTime().toString();
+		form.endTime = task.getEndTime().toString();
+		form.categoryId = task.getCategory().getId();
 		form.description = task.getDescription();
 		return form;
 	}
